a8a05c13244b3927d17cabdf9ce40d7678bb8b35
a47894785 <a47894785@gmail.com>
2022-06-24 23:57:23 +0800

    Drwer

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NativeBaseProvider } from "native-base";
+
+import COLORS from "./theme/theme";
+import Tasks from "./screens/Tasks";
+import About from "./screens/About";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Drawer.Navigator
+          initialRouteName="Tasks"
+          screenOptions={{
+            drawerStyle: {
+              width: "65%",
+              backgroundColor: COLORS.primaryVariant
+            },
+            drawerType: "back",
+            overlayColor: "transparent",
+            headerShown: false
+          }}
+        >
+          <Drawer.Screen name="Tasks" component={Tasks} />
+          <Drawer.Screen name="About" component={About} />
+        </Drawer.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ theme/theme.js
@@ -0,0 +1,19 @@
+const primary = "#2A7B99";
+const primaryVariant = "#69AFCE";
+const secondary = "#A4DEF1";
+const white = "#ffffff";
+const blueGray = "#132932";
+const dark = "#1F2325";
+const red = "#E16160";
+
+const COLORS = {
+  primary,
+  primaryVariant,
+  secondary,
+  white,
+  blueGray,
+  dark,
+  red
+};
+
+export default COLORS;
